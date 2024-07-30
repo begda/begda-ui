dd0f5287dedbcec1c1c99b1a5d2104dbc1cb4e27
liaohui1080 <liaohui1080@tianliaohui.com>
2024-07-30 16:59:39 +0800

    重新开发了platforms的代码用ai做了优化,解决了再node环境下检测不到navigator造成的报错问题

--- packages/plateform/src/platforms.js
+++ packages/plateform/src/platforms.js
@@ -1,148 +1,178 @@
-/*
+/**
+ * 平台检测模块
+ * 此模块用于检测当前运行环境（浏览器或Node.js）并确定具体平台类型，
+ * 如微信小程序、Android、iPhone等。在浏览器环境中，通过分析navigator.userAgent
+ * 来识别平台；在Node.js环境中，由于navigator不存在，返回空对象。
+ */
 
-//使用例子
-import {Platforms,PlatformsSync} from "@/assets/utils/platforms";
-// console.log(baPlatform)
-const nowPlatfoem = ref()
-//组合试用法
-Platforms.miniProgram(() => {
-  nowPlatfoem.value = '这个要在小程序显示的 字段'
-  Platforms.Android(() => {
-    nowPlatfoem.value = '这个要在 安卓小程序显示的 字段'
-  })
-  Platforms.iPhone(() => {
-    nowPlatfoem.value = '只在iphone下运行'
-  })
-})
-Platforms.iPhone(() => {
-  nowPlatfoem.value = '只在iphone下运行'
-})
-Platforms.Mac(() => {
-  nowPlatfoem.value = '只在mac下运行'
-})
-Platforms.Desk(() => {
-  nowPlatfoem.value = '只在桌面环境下运行'
-})
+let _Platforms = {};
+let _platformSync = {};
+/**
+ * 在浏览器环境中，定义平台检测逻辑
+ * @param {Function} callback 回调函数，接收平台类型作为参数
+ */
+function detectPlatform(callback = () => {}) {
+  // 获取User-Agent字符串
+  const sUsrAg = navigator.userAgent;
 
-* */
+  // 判断是否为微信小程序环境
+  const _miniProgram =
+    sUsrAg.indexOf("miniProgram") > -1 ? "miniProgram" : false;
 
-let sUsrAg = navigator.userAgent;
-if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
-  // 浏览器环境下的代码
-  sUsrAg = "";
-}
+  // 判断是否为Html5Plus环境（uni-app外壳）
+  const _Html5Plus = sUsrAg.indexOf("Html5Plus") > -1 ? "Html5Plus" : false;
 
-//所在平台供应商
-const _miniProgram = sUsrAg.indexOf("miniProgram") > 1 ? "miniProgram" : false; //微信小程序平台
-const _Html5Plus = sUsrAg.indexOf("Html5Plus") > 1 ? "Html5Plus" : false; //uniapp外壳平台
-const _Wechat = !!sUsrAg.match(/MicroMessenger/i) ? "Wechat" : false; //微信
+  // 判断是否为微信环境
+  const _Wechat = !!sUsrAg.match(/MicroMessenger/i) ? "Wechat" : false;
 
-//所在系统
-const _iPhone = sUsrAg.indexOf("iPhone") > 1 ? "iPhone" : false;
-const _Android = sUsrAg.indexOf("Android") > 1 ? "Android" : false;
-const _Windows =
-  !!sUsrAg.match(/compatible/i) || sUsrAg.match(/Windows/i) ? "Windows" : false;
-const _Mac =
-  !!sUsrAg.match(/Macintosh/i) || sUsrAg.match(/Macintosh/i) ? "Mac" : false;
+  // 判断是否为iPhone环境
+  const _iPhone = sUsrAg.indexOf("iPhone") > -1 ? "iPhone" : false;
 
-const platform = function (callback = () => {}) {
+  // 判断是否为Android环境
+  const _Android = sUsrAg.indexOf("Android") > -1 ? "Android" : false;
+
+  // 判断是否为Windows环境
+  const _Windows =
+    !!sUsrAg.match(/compatible/i) || sUsrAg.match(/Windows/i)
+      ? "Windows"
+      : false;
+
+  // 判断是否为Mac环境
+  const _Mac = !!sUsrAg.match(/Macintosh/i) ? "Mac" : false;
+
+  // 根据检测结果，调用回调函数并传递平台类型
   if (_miniProgram) {
     callback("miniProgram");
     return "miniProgram";
-  }
-  if (_Html5Plus) {
+  } else if (_Html5Plus) {
     callback("Html5Plus");
     return "Html5Plus";
-  }
-  if (_Android) {
+  } else if (_Android) {
     callback("Android");
     return "Android";
-  }
-  if (_iPhone) {
+  } else if (_iPhone) {
     callback("iPhone");
     return "iPhone";
-  }
-  if (_Windows) {
+  } else if (_Windows) {
     callback("Windows");
     return "Windows";
-  }
-  if (_Mac) {
+  } else if (_Mac) {
     callback("Mac");
     return "Mac";
+  } else if (_Mac || _Windows) {
+    callback("Desk"); // 如果是Mac或Windows，则视为桌面环境
+    return "Desk";
+  } else {
+    callback(false); // 无法识别的环境，返回false
   }
-  if (_Mac || _Windows) {
-    callback("Desk");
-  }
-
   //不在识别返回的就返回false
   return false;
-};
-
-function miniProgram(callback) {
-  platform((res) => {
-    if (res === "miniProgram") {
-      callback(res);
-    }
-  });
 }
 
-function Html5Plus(callback) {
-  platform((res) => {
-    if (res === "Html5Plus") {
-      callback(res);
-    }
-  });
+/**
+ * 创建平台API对象，封装各种平台的检测方法
+ * @returns {Object} 包含各种平台检测方法的对象
+ */
+function createPlatformApi() {
+  return {
+    /**
+     * 检测是否为微信小程序环境
+     * @param {Function} callback 回调函数，接收平台类型作为参数
+     */
+    miniProgram(callback) {
+      detectPlatform((res) => {
+        if (res === "miniProgram") {
+          callback(res);
+        }
+      });
+    },
+
+    /**
+     * 检测是否为Html5Plus环境
+     * @param {Function} callback 回调函数，接收平台类型作为参数
+     */
+    Html5Plus(callback) {
+      detectPlatform((res) => {
+        if (res === "Html5Plus") {
+          callback(res);
+        }
+      });
+    },
+
+    /**
+     * 检测是否为Android环境
+     * @param {Function} callback 回调函数，接收平台类型作为参数
+     */
+    Android(callback) {
+      detectPlatform((res) => {
+        if (res === "Android") {
+          callback(res);
+        }
+      });
+    },
+
+    /**
+     * 检测是否为iPhone环境
+     * @param {Function} callback 回调函数，接收平台类型作为参数
+     */
+    iPhone(callback) {
+      detectPlatform((res) => {
+        if (res === "iPhone") {
+          callback(res);
+        }
+      });
+    },
+
+    /**
+     * 检测是否为Windows环境
+     * @param {Function} callback 回调函数，接收平台类型作为参数
+     */
+    Windows(callback) {
+      detectPlatform((res) => {
+        if (res === "Windows") {
+          callback(res);
+        }
+      });
+    },
+
+    /**
+     * 检测是否为Mac环境
+     * @param {Function} callback 回调函数，接收平台类型作为参数
+     */
+    Mac(callback) {
+      detectPlatform((res) => {
+        if (res === "Mac") {
+          callback(res);
+        }
+      });
+    },
+
+    /**
+     * 检测是否为桌面环境（Mac或Windows）
+     * @param {Function} callback 回调函数，接收平台类型作为参数
+     */
+    Desk(callback) {
+      detectPlatform((res) => {
+        if (res === "Mac" || res === "Windows") {
+          callback("Desk");
+        }
+      });
+    },
+  };
 }
 
-function Android(callback) {
-  platform((res) => {
-    if (res === "Android") {
-      callback(res);
-    }
-  });
+// 检查当前环境是否为浏览器环境
+if (typeof navigator !== "undefined") {
+  // 如果是浏览器环境，创建并导出平台API对象
+  _Platforms = createPlatformApi();
+  _platformSync = detectPlatform(() => {}); //同步获取运行平台
+} else {
+  // 如果是Node.js环境，打印提示信息并返回空对象
+  console.log("运行在 Node.js, navigator 是不存在的.");
+  _Platforms = false;
+  _platformSync = false;
 }
 
-function iPhone(callback) {
-  platform((res) => {
-    if (res === "iPhone") {
-      callback(res);
-    }
-  });
-}
-
-function Windows(callback) {
-  platform((res) => {
-    if (res === "Windows") {
-      callback(res);
-    }
-  });
-}
-
-function Mac(callback) {
-  platform((res) => {
-    if (res === "Mac") {
-      callback(res);
-    }
-  });
-}
-
-//桌面浏览器环境下运行,不依赖任何外壳
-function Desk(callback) {
-  platform((res) => {
-    if (res === "Mac" || res === "Windows") {
-      callback("Desk");
-    }
-  });
-}
-
-export const PlatformsSync = platform(() => {});
-//这个直接返回当前运行环境,一次只能返回一个
-export const Platforms = {
-  Android,
-  miniProgram,
-  Html5Plus,
-  iPhone,
-  Windows,
-  Mac,
-  Desk,
-}; //返回回调组合运行环境
+// 导出平台检测模块
+export const Platforms = _Platforms;
+export const PlatformsSync = _platformSync;
